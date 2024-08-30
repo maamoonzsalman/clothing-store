ba07be8547d7f2b8929cc2778531236a2a6fc8d2
Maamoon Salman <137584309+maamoonzsalman@users.noreply.github.com>
2024-08-30 14:53:03 -0700

    Initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import '../styles/Footer.css';
+
+function Footer() {
+    return (
+        <div>
+            hey
+        </div>
+    )
+}
+
+export default Footer;
---
+++ src/components/NavBar.js
@@ -0,0 +1,22 @@
+import '../styles/NavBar.css';
+
+function NavBar () {
+    return (
+        <div className='nav-bar'>
+            <div className='nav-item'>
+                    Maamoon's Shop
+            </div> 
+            <div className="nav-item">
+                    Home
+            </div>
+            <div className="nav-item">
+                    Shop
+            </div>
+            <div className='nav-item'>
+                    Cart Logo
+            </div>
+       </div>
+    );
+};
+
+export default NavBar;
